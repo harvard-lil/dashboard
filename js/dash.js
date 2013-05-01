436a96c136afe3f46ed2941dd1fec06f654a8903
Ingrid Cheung <hi@ingridcheung.com>
2013-05-01 10:09:46 -0400

    I BROKE IT.

--- js/dash.js
+++ js/dash.js
@@ -3,16 +3,18 @@
     IssuesEvent : "opened an issue on",
     CreateEvent : "created repo",
     PushEvent : "pushed to repo",
-    ForkEvent : "forked repo"
+    ForkEvent : "forked repo",
+    DeleteEvent : "deleted repo"
   }
   var org = 'harvard-lil'
-  var container = $("#github-module");
+  var container = $("#github-activity");
   var data = $.getJSON('https://api.github.com/orgs/' + org + '/events', function(data){
     $.each(data, function(){
       var dict = {
         name : this.actor.login,
         activity : action_map[this.type],
-        repo : this.repo.name
+        repo : this.repo.name,
+        repo_url : this.repo.url
       };
       var action = ich.action(dict);
       container.append(action);
