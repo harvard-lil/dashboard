008f6631a82131f9e32d8b0116f03cd0d9425d4b
Ingrid <hi@ingridcheung.com>
2013-05-02 08:11:42 -0400

    Checkpoint.

--- js/dash.js
+++ js/dash.js
@@ -1,23 +1,145 @@
 (function(){
   var action_map = {
-    IssuesEvent : "opened an issue on",
-    CreateEvent : "created repo",
-    PushEvent : "pushed to repo",
-    ForkEvent : "forked repo",
-    DeleteEvent : "deleted repo"
+    CommitCommentEvent : {
+      item_type : "",
+      pre_action : "",
+      action_item : "",
+      post_action : ""
+      "#",
+    },
+    CreateEvent : {
+      item_type : "",
+      pre_action : "",
+      action_item : "",
+      post_action : ""
+      "created repo",
+    },
+    DeleteEvent : {
+      item_type : "",
+      pre_action : "",
+      action_item : "",
+      post_action : ""
+      "deleted repo",
+    },
+    DownloadEvent : {
+      item_type : "",
+      pre_action : "",
+      action_item : "",
+      post_action : ""
+      "#",
+    },
+    FollowEvent : {
+      item_type : "",
+      pre_action : "",
+      action_item : "",
+      post_action : ""
+      "followed repo",
+    },
+    ForkEvent : {
+      item_type : "",
+      pre_action : "",
+      action_item : "",
+      post_action : ""
+      "forked repo",
+    },
+    ForkApplyEvent : {
+      item_type : "",
+      pre_action : "",
+      action_item : "",
+      post_action : ""
+      "#",
+    },
+    GistEvent : {
+      item_type : "",
+      pre_action : "",
+      action_item : "",
+      post_action : ""
+      "#",
+    },
+    GollumEvent : {
+      item_type : "",
+      pre_action : "",
+      action_item : "",
+      post_action : ""
+      "#",
+    },
+    IssueCommentEvent : {
+      item_type : "",
+      pre_action : "",
+      action_item : "",
+      post_action : ""
+      "#",
+    },
+    IssuesEvent : {
+      item_type : "",
+      pre_action : "",
+      action_item : "",
+      post_action : ""
+      'opened an issue on',
+    },
+    MemberEvent : {
+      item_type : "",
+      pre_action : "",
+      action_item : "",
+      post_action : ""
+      "#",
+    },
+    PublicEvent : {
+      item_type : "",
+      pre_action : "",
+      action_item : "",
+      post_action : ""
+      "#",
+    },
+    PullRequestEvent : {
+      item_type : "",
+      pre_action : "",
+      action_item : "",
+      post_action : ""
+      "#",
+    },
+    PullRequestReviewCommentEvent : {
+      item_type : "",
+      pre_action : "",
+      action_item : "",
+      post_action : ""
+      "#",
+    },
+    PushEvent : {
+      item_type : "",
+      pre_action : "pushed to ",
+      action_item : "repo",
+      post_action : ""
+    },
+    TeamAddEvent : {
+      item_type : "",
+      pre_action : "",
+      action_item : "",
+      post_action : ""
+    },
+    WatchEvent : {
+      item_type : "",
+      pre_action : "",
+      action_item : "",
+      post_action : ""
+    },
   }
   var org = 'harvard-lil'
   var container = $("#github-activity");
   var data = $.getJSON('http://localhost:8888/github', function(data){
     $.each(data, function(){
+      var keys = action_map[this.type]
       var dict = {
-        name : this.actor.login,
-        activity : action_map[this.type],
+        user : this.actor.login,
+        pre_action : "!",
+        activity_url : this.payload[keys['type']].['html_url'],
+        activity : this.,
+        post_action : "!",
         repo : this.repo.name,
         repo_url : this.repo.url
       };
-      var action = ich.action(dict);
-      container.append(action);
+      var activity = ich.activity(dict);
+      container.append(activity);
     });
   });
 })();
