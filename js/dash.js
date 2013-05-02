6e2270e38b9ba47b4499e9c2d209d3899461b401
ingrid <hi@ingridcheung.com>
2013-05-02 10:03:24 -0400

    Only some github events work now, checkpoint.

--- js/dash.js
+++ js/dash.js
@@ -1,19 +1,22 @@
 (function(){
   var action_map = {
+     /** /
     CommitCommentEvent : {
-      item_type : "",
-      pre_action : "",
-      action_item : "",
-      post_action : ""
-      "#",
+      pre_action : " posted a",
+      item : " comment",
+      post_action : " on",
+      item_url : "object",
+      description : "object"
     },
+    /**/
     CreateEvent : {
-      item_type : "",
-      pre_action : "",
-      action_item : "",
-      post_action : ""
-      "created repo",
+      pre_action : " created repo",
+      item : "",
+      post_action : "",
+      item_url : "",
+      description : ""
     },
+     /** /
     DeleteEvent : {
       item_type : "",
       pre_action : "",
@@ -122,24 +125,28 @@
       pre_action : "",
       action_item : "",
       post_action : ""
-    },
+    }
+     /**/
   }
   var org = 'harvard-lil'
   var container = $("#github-activity");
   var data = $.getJSON('http://localhost:8888/github', function(data){
     $.each(data, function(){
       var keys = action_map[this.type]
-      var dict = {
-        user : this.actor.login,
-        pre_action : "!",
-        activity_url : this.payload[keys['type']].['html_url'],
-        activity : this.,
-        post_action : "!",
-        repo : this.repo.name,
-        repo_url : this.repo.url
-      };
-      var activity = ich.activity(dict);
+      if(keys!==undefined){
+          var dict = {
+              user : this.actor.login,
+              pre_action : keys.pre_action,
+              post_action : keys.post_action,
+              repo : this.repo.name,
+              repo_url : this.repo.url
+          };
+          // Build item.
+          //        dict.item_url = this.payload[keys['type']].['html_url'],
+          //dict.item = this.,
+          var activity = ich.activity(dict);
       container.append(activity);
+      }
     });
   });
 })();
