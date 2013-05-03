30723661df24d66f07a3fd94b12c5f6a57364e25
ingrid <hi@ingridcheung.com>
2013-05-03 14:39:21 -0400

    Pulling in google analytics stuff.

--- server/server.py
+++ server/server.py
@@ -1,15 +1,17 @@
 import tornado.ioloop
 import tornado.web
 import jwt
-import config
+import sys
 
 from requests import post, get
+from apiclient.errors import HttpError
+from oauth2client.client import AccessTokenRefreshError
+
+import config
+import util
 
 ORG = 'harvard-lil'
-
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
+TABLE_ID = ''
 
 class GitHubHandler(tornado.web.RequestHandler):
     def get(self):
@@ -23,9 +25,31 @@ class GitHubHandler(tornado.web.RequestHandler):
         else:
             raise Exception("Github error.")
 
+
+
 class AnalyticsHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello world.")
+        service = util.initialize_service()
+        try:
+            results = service.data().ga().get(
+                ids=TABLE_ID,
+                start_date='2012-01-01',
+                end_date='2013-05-02',
+                metrics='ga:visits',
+                dimensions='ga:source,ga:keyword',
+                sort='-ga:visits',
+                filters='ga:medium==organic',
+                start_index='1',
+                max_results='25').execute()
+            self.write(results)
+        except TypeError, error:
+            print ('There was an error in constructing your query : %s' % error)
+        except HttpError, error:
+            print ('Arg, there was an API error : %s : %s' %
+                   (error.resp.status, error._get_reason()))
+        except AccessTokenRefreshError:
+            print ('The credentials have been revoked or expired, please re-run '
+                   'the application to re-authorize')
 
 application = tornado.web.Application([
     (r"/analytics", AnalyticsHandler),
