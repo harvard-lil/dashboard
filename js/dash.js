e6330da6457fce33c38d76ecd1c6d82d269520be
ingrid <hi@ingridcheung.com>
2013-05-03 15:58:46 -0400

    Scrum graph added.

--- js/dash.js
+++ js/dash.js
@@ -12,8 +12,27 @@ require(["eventcat"], function(cat) {
       });
   });
 
-  var scrum_data = [0, 0, 0, 1, 2, 3, 4, 5, 5, 5, 0];
+  var scrum_data = [0, 0, 0, 1, 2, 3, 4, 5, 5, 5, 0,3,4, 5, 2, 5, 3, 4, 2, 4, 1, 1, 1, 5, 4, 3, 4, 3, 4, 4, 4, 5, 5, 5, 2, 3, 2, 2, 2, 4, 4, 3, 2, 5, 4, 3, 2];
+
+  // We could generate this programatically if needed.
+  // I assume a 1 person scrum should probably count as much as a 0 person scrum.
+  // Cuurently these colors are just shuffled versions of the github colors.
+  var color_map = {
+    0 : "#eee",
+    1 : "#eee",
+    2 : "#85d6e6",
+    3 : "#658cc6",
+    4 : "#4044a3",
+    5 : "#231e68"
+  };
 
   // D3 stuff.
-  d3.select("#scrum_graph").text("New paragraph!");
+  d3.select("#scrum_graph").selectAll("div")
+    .data(scrum_data)
+    .enter()
+    .append("div")
+    .attr("class", "day")
+    .style("background-color", function(d) {
+      return color_map[d];
+    });
 });
